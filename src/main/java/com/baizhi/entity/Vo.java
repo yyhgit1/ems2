ae22b16c083896f35a955b799e4651ec3857e980
yyh <1147962176@qq.com>
2019-12-08 23:36:59 +0800

    创建辅助实体

---
+++ src/main/java/com/baizhi/entity/Vo.java
@@ -0,0 +1,18 @@
+package com.baizhi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class Vo implements Serializable {
+    List<Dept> depts;
+    List<Emp> emps;
+}
