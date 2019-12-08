a1f5e737f127d1f6af27ce3293712340e985acbd
yyh <1147962176@qq.com>
2019-12-08 23:35:38 +0800

    创建部门实体

---
+++ src/main/java/com/baizhi/entity/Dept.java
@@ -0,0 +1,17 @@
+package com.baizhi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@Data
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dept implements Serializable {
+    private String id;
+    private String name;
+}
